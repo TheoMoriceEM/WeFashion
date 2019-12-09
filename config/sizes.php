b8288b6753930b97422de004896a27ef6ea878db
TheoMoriceEM <theo.morice5@gmail.com>
2019-12-09 18:05:32 +0100

    products table seeder

---
+++ config/sizes.php
@@ -0,0 +1,4 @@
+<?php
+
+// Array for stocking clothes sizes
+return ['XS', 'S', 'M', 'L', 'XL'];
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence(),
+        'description' => $faker->paragraph(),
+        'price' => $faker->randomFloat(2, 1, 300),
+        'sizes' => implode(',', collect(config('sizes'))->shuffle()->slice(0, rand(1, 4))->toArray()),
+        'published' => rand(0, 1),
+        'discount' => rand(0, 1),
+        'reference' => $faker->shuffle('0123456789abcdef'),
+        'category_id' => rand(1, 2)
+    ];
+});
