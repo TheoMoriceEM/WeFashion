1e6351f917d7df8892a0501d6ecde6933cace557
TheoMoriceEM <theo.morice5@gmail.com>
2019-12-09 16:36:00 +0100

    categories table seeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class CategoriesTableSeeder extends Seeder
+{
+    private $table = 'categories';
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table($this->table)->insert([
+            'name' => 'Homme',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table($this->table)->insert([
+            'name' => 'Femme',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
