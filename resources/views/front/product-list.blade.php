9e8364b1710e917b3cbb6f959f72505a7c396f09
TheoMoriceEM <theo.morice5@gmail.com>
2019-12-10 18:37:20 +0100

    product list

---
+++ resources/views/front/product-list.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.master')
+
+@section('CSS')
+
+@endsection
+
+@section('content')
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12">
+                <p class="text-right">4 résultats</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <div class="product-img">
+                        <img src="https://picsum.photos/400" alt="lorem ipsum" class="w-100">
+                    </div>
+                    <p class="product-name m-0">Lorem ipsum</p>
+                    <p class="product-price">59.65€</p>
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <div class="product-img">
+                        <img src="https://picsum.photos/400" alt="lorem ipsum" class="w-100">
+                    </div>
+                    <p class="product-name m-0">Lorem ipsum</p>
+                    <p class="product-price">59.65€</p>
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <div class="product-img">
+                        <img src="https://picsum.photos/400" alt="lorem ipsum" class="w-100">
+                    </div>
+                    <p class="product-name m-0">Lorem ipsum</p>
+                    <p class="product-price">59.65€</p>
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <div class="product-img">
+                        <img src="https://picsum.photos/400" alt="lorem ipsum" class="w-100">
+                    </div>
+                    <p class="product-name m-0">Lorem ipsum</p>
+                    <p class="product-price">59.65€</p>
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <div class="product-img">
+                        <img src="https://picsum.photos/400" alt="lorem ipsum" class="w-100">
+                    </div>
+                    <p class="product-name m-0">Lorem ipsum</p>
+                    <p class="product-price">59.65€</p>
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <div class="product-img">
+                        <img src="https://picsum.photos/400" alt="lorem ipsum" class="w-100">
+                    </div>
+                    <p class="product-name m-0">Lorem ipsum</p>
+                    <p class="product-price">59.65€</p>
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
